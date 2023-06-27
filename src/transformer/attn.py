8af501370b35555e4ea910cc742aeefd71703ded
dawnik17 <135340243+dawnik17@users.noreply.github.com>
2023-06-27 17:58:09 +0530

    Multi Query Attention

--- src/transformer/attn.py
+++ src/transformer/attn.py
@@ -93,3 +93,69 @@ class MultiHeadedAttention(nn.Module):
         # (QKtranspose).value
         # shape - [Batch, Heads, SeqLen, Dimension // Heads]
         return torch.matmul(qk, value)
+
+
+class MultiQueryAttention(nn.Module):
+    def __init__(self, dimension, heads, dropout=0.0):
+        super().__init__()
+        self.heads = heads
+        self.dimension = dimension
+        
+        self.head_dim = self.dimension // self.heads
+        self.qkv = nn.Linear(dimension, dimension + 2 * self.head_dim)
+        self.outl = nn.Linear(dimension, dimension)
+        
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        """
+        MQA is only for Self-Attention
+        x - [batch, seqlen, dimension]
+        """
+        assert self.dimension == x.size(-1)
+        batch_size = x.size(0)
+        
+        x = self.qkv(x)
+        query, key, value = torch.split(x, split_size_or_sections=[self.dimension, self.head_dim, self.head_dim], dim=-1)
+
+        # query - [batch, seqlen, dimension]
+        # key, value - [batch, seqlen, head_dim]
+        attn = self.attention(query, key, value, mask, self.dropout)
+        return self.outl(attn)
+
+
+    def attention(self, query, key, value, mask=None, dropout=None):
+        # query - [batch, seqlen, dimension]
+        # key, value - [batch, seqlen, head_dim]
+        
+        batch_size = query.size(0)
+        query_length = query.size(1)
+        key_length = key.size(1)
+        
+        attn_shape = (batch_size, self.heads, query_length, key_length)
+        
+        # query from - [batch, seqlen, dimension] -> [batch, seqlen * heads, head_dim]
+        query_shape = query.shape
+        query = query.reshape(batch_size, self.heads * query_length, self.head_dim)
+        
+        # query - [batch, seqlen * heads, head_dim]
+        # key - [batch, seqlen, head_dim]
+        # q.k_t - [batch, seqlen * heads, seqlen]
+        # (q.k_t).view(attn_shape) - [batch, heads, seqlen, seqlen]
+        qk = torch.div(
+            torch.matmul(query, key.transpose(-2, -1)), math.sqrt(self.head_dim)
+        ).view(attn_shape)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+            qk = qk.masked_fill(mask == 0, -1e9)
+
+        qk = nn.Softmax(dim=-1)(qk)
+        qk = self.dropout(qk) if dropout is not None else qk
+
+        # qk from - [batch, heads, seqlen, seqlen] -> [batch, heads * seqlen, seqlen]
+        # value - [batch, seqlen, head_dim]
+        # torch.matmul(qk, value) -> [batch, heads * seqlen, head_dim]
+        # torch.matmul(qk, value).view(query_shape) - [batch, seqlen, dimension]
+        qk = qk.view(batch_size, self.heads * query_length, key_length)
+        return torch.matmul(qk, value).view(query_shape) 
