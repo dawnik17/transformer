ae21568b9503c59798c625828da8f516ad983cb0
dawnik17 <135340243+dawnik17@users.noreply.github.com>
2023-09-16 20:47:23 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transformer',  # Replace with your package name
+    version='0.1',       # Replace with your package version
+    packages=find_packages(),
+    install_requires=[],  # List your package dependencies here
+)
