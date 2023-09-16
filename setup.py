e5521a0a5ec603a639f7cf74401603cad16c8b45
dawnik17 <135340243+dawnik17@users.noreply.github.com>
2023-09-16 20:51:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup, Extension
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='transformer',  # Replace with your package name
-    version='0.1',       # Replace with your package version
-    packages=find_packages(),
-    install_requires=[],  # List your package dependencies here
+    name="transformer",
+    version="1.0",
+    description="Transformer",
+    url="https://github.com/dawnik17/transformer",
+    author="Dawnik17",
+    author_email="dawnik17@gmail.com",
+    license="unlicense",
+    zip_safe=False,
+    packages=["src"],
+    include_package_data=True,
+    classifiers=["Programming Language :: Python :: 3"],
+    long_description=long_description
 )
